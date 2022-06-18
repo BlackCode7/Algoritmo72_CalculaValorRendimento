609714f9ed44e3e624fb53c3c821e5ec506f1b54
Anderson <BlackCode7@outlook.com>
2022-06-17 22:27:40 -0300

    criando função que calcula o juros create function calculate interest

--- src/Algoritmo72_CalculaValorRendimento/Algoritmo72CalculaValorRendimento/Algoritmo72CalculaValorRendimento.java
+++ src/Algoritmo72_CalculaValorRendimento/Algoritmo72CalculaValorRendimento/Algoritmo72CalculaValorRendimento.java
@@ -1,10 +1,29 @@
 package Algoritmo72_CalculaValorRendimento.Algoritmo72CalculaValorRendimento;
 
+import java.util.Scanner;
+
 public class Algoritmo72CalculaValorRendimento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Algoritmo72CalculaValorRendimento();
+	}
+	
+	public static void Algoritmo72CalculaValorRendimento() {
+		/*Criar um algoritmo que leia o valor de um depósito e o valor da taxa de 
+		  juros. Calcular e imprimir o valor do rendimento e o valor total depois 
+		  do rendimento.  deposito, taxa, valor, total;*/
+		
+		Scanner scanner = new Scanner(System.in);
+		double deposito = scanner.nextDouble();
+		double taxa = scanner.nextDouble();
+		double valor;
+		double total;
+		
+		valor = deposito * taxa / 100;
+		total = deposito + valor;
+		
+		System.out.println("Rendimentos: "+valor+" Total: "+total);
+		
 	}
 
 }
