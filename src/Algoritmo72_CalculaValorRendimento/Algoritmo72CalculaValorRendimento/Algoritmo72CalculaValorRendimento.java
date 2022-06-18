1abc160d34265f8a1892c104780b14619519b4ee
Anderson <BlackCode7@outlook.com>
2022-06-17 22:18:30 -0300

    first commit of the project algorithm

---
+++ src/Algoritmo72_CalculaValorRendimento/Algoritmo72CalculaValorRendimento/Algoritmo72CalculaValorRendimento.java
@@ -0,0 +1,10 @@
+package Algoritmo72_CalculaValorRendimento.Algoritmo72CalculaValorRendimento;
+
+public class Algoritmo72CalculaValorRendimento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
